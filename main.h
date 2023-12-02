08eb6bdad2ecbc19604756c786337dc6121510ab
omayma777emad <omimaemad777@gmail.com>
2023-12-02 07:28:17 -0800

    main

--- main.h
+++ main.h
@@ -4,6 +4,36 @@
 #include <stdio.h>;
 #include <stdlib.h>;
 #include <stdarg.h>;
+
+
+
+/**
+ * struct data - Struct op
+ *
+ * @data: The format.
+ * @f: The function associated.
+ */
+
+struct data
+{
+	char data;
+	int (*f)(va_list, char[]);
+};
+
+
+/**
+ * typedef struct data data_type - Struct op
+ *
+ * @data: The format.
+ * @data_type: The function associated.
+ */
+typedef struct data data_type;
+
+int _printf(const char *format, ...);
+int handle_print(const char *data, va_list args, char buffer[]);
+
+/* functions Prototypes */
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int handle-content(char *format, va_list args, char content[]);
